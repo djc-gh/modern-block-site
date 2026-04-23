efeb7b597cb7e8ce323a59c59675b683be5b1917
Daniel Chentiwuni Joshua <danieljoshuachentiwuni1@gmail.com>
2026-04-23 14:06:58 +0000

    Add STATICFILES_STORAGE for Whitenoise

--- config/settings.py
+++ config/settings.py
@@ -13,6 +13,8 @@ DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
 # Application definition
 INSTALLED_APPS = [
     # Django apps
